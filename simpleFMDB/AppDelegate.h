ccef6f0306860e6ca4febd4e2975e8c18d6b30d5
jinshuaier <470401910@qq.com>
2017-08-15 17:14:16 +0800

    Initial Commit

---
+++ simpleFMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  simpleFMDB
+//
+//  Created by 胡高广 on 2017/8/15.
+//  Copyright © 2017年 胡高广. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ simpleFMDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simpleFMDB
+//
+//  Created by 胡高广 on 2017/8/15.
+//  Copyright © 2017年 胡高广. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
